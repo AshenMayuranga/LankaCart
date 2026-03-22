2ac77f979a60182054d25ef1fd34c7de46974eb4
AshenMayuranga <ashen.ayuranga@gmail.com>
2026-03-22 13:36:26 +0530

    add initial project files and configurations2

--- lankacart-frontend/vite.config.ts
+++ lankacart-frontend/vite.config.ts
@@ -10,10 +10,10 @@ export default defineConfig({
   server: {
     port: 5173,
     proxy: {
-      '/users': { target: 'http://localhost:8085', changeOrigin: true },
-      '/products': { target: 'http://localhost:8085', changeOrigin: true },
-      '/orders': { target: 'http://localhost:8085', changeOrigin: true },
-      '/inventory': { target: 'http://localhost:8085', changeOrigin: true },
+      '/users': { target: 'http://localhost:8086', changeOrigin: true },
+      '/products': { target: 'http://localhost:8086', changeOrigin: true },
+      '/orders': { target: 'http://localhost:8086', changeOrigin: true },
+      '/inventory': { target: 'http://localhost:8086', changeOrigin: true },
     },
   },
 })
